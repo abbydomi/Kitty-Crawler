8dcabb4715dec0bdab80d49cd46da21579d45f58
Abby Dominguez <abbytorade@icloud.com>
2024-10-17 19:15:27 +0200

    Initial Commit

---
+++ Kitty Crawler/Kitty_CrawlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kitty_CrawlerApp.swift
+//  Kitty Crawler
+//
+//  Created by Abby Dominguez on 17/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Kitty_CrawlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
